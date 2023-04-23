8e21fd38dce427b52ef865d785d8903f5034d3ee
Egor <egor_onishchuk@mail.ru>
2023-04-23 15:45:42 +0300

    Добавлен ввод нескольких значений в модуль ввода данных

--- input.py
+++ input.py
@@ -7,3 +7,6 @@ def get_sequence():
     for i in range(1, length + 1):
         sequence.append(int(input(f'Введите {i} число. ')))
     return sequence
+
+def get_values(elements):
+    return [i for i in input(f'Введите {elements}. ').split()]
