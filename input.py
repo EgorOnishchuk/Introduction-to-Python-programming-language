d19999413fa8a38975053ac141c4c1da5f89ef01
Egor <egor_onishchuk@mail.ru>
2023-04-23 14:44:20 +0300

    Добавлен модуль ввода данных

---
+++ input.py
@@ -0,0 +1,9 @@
+def get_value(element):
+    return input(f'Введите {element}. ')
+
+def get_sequence():
+    sequence = list()
+    length = int(input('Введите количество чисел. '))
+    for i in range(1, length + 1):
+        sequence.append(int(input(f'Введите {i} число. ')))
+    return sequence
