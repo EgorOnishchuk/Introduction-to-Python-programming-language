27145698bb0a4342de1e5da447434cd3189a07e9
Egor <egor_onishchuk@mail.ru>
2023-05-22 17:00:58 +0300

    Добавлена документация модуля ввода данных

--- input.py
+++ input.py
@@ -1,7 +1,11 @@
+"""Позволяет получать значения, введённые пользователем."""
+
 def get_value(element):
+    """Возвращает значение элемента, введённое пользователем."""
     return input(f'Введите {element}. ')
 
 def get_sequence():
+    """Возвращает список чисел, введённый пользователем."""
     sequence = list()
     length = int(input('Введите количество чисел. '))
     for i in range(1, length + 1):
@@ -9,4 +13,5 @@ def get_sequence():
     return sequence
 
 def get_values(elements):
+    """Возвращает список значений элементов, введённый пользователем на одной строке через пробел."""
     return [i for i in input(f'Введите {elements}. ').split()]
